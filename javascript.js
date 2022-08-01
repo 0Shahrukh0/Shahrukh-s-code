c093b0ec073162b2b2a9fadf5924bee877a1ccfc
0Shahrukh0 <110408913+0Shahrukh0@users.noreply.github.com>
2022-08-01 20:08:34 +0500

    This is my first project.
    I have copied the first template of W3school.com. I want to make the code shorter and more efficient.

---
+++ javascript.js
@@ -0,0 +1,58 @@
+// let homeBtn = document.getElementById("homebtn")
+
+// homeBtn.addEventListener("click", function(){
+//     // homeBtn.innerHTML = "www"
+//     // console.log("clicked")
+    
+// })
+
+function homebtn()
+{
+    let homeBtn = document.getElementById("homebtn")
+    homeBtn.innerHTML = "www"
+    // homeBtn.innerHTML =  `<button id="homebtn" , class="btn" , onclick="homebtn()", style= "background-color: purple; "> Home </button>`
+    console.log("clicked")
+}
+
+function bandbtn(){
+    let bandBtn = document.getElementById("bandbtn")
+    bandBtn.innerHTML = "www"
+    console.log("clicked")
+}
+
+function tourbtn(){
+    let tourbtn = document.getElementById("tourbtn")
+    tourbtn.innerHTML = "www"
+    console.log("clicked")
+
+}
+
+function contactbtn(){
+    let contactbtn = document.getElementById("contactbtn")
+    contactbtn.innerHTML = "www"
+    console.log("clicked")
+}
+
+
+function morebtn(){
+    let morebtn = document.getElementById("morebtn")
+    morebtn.innerHTML = "www"
+    console.log("clicked")
+}
+
+function searchbtn(){
+    let searchbtn = document.getElementById("searchbtn")
+    searchbtn.innerHTML = "www"
+    console.log("clicked")
+}
+
+
+ 
+
+ function sendbtn(){
+    let nameInput= document.getElementById("nameinput")
+    let emailInput= document.getElementById("emailinput")
+    console.log ("Name is: " + nameInput)
+console.log ("Email is: "+ emailInput.textContent)
+console.log("button clicked") 
+}
